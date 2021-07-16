2a07e77d6f8807be161a3ddb2a0929e0c4925a38
nlom0218 <nlom0218@naver.com>
2021-07-16 23:20:26 +0900

    me query

---
+++ src/user/me/me.resolvers.js
@@ -0,0 +1,12 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Query: {
+    me: protectedResolver((_, __, { loggedInUser }) => {
+      return client.user.findUnique({
+        where: { id: loggedInUser.id }
+      })
+    })
+  }
+}
