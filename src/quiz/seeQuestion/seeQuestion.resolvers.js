f9d5b2190e94a992103eebf6be261987d94a7263
nlom0218 <nlom0218@naver.com>
2021-08-01 16:01:17 +0900

    seeQuiz type tags

--- src/quiz/seeQuestion/seeQuestion.resolvers.js
+++ src/quiz/seeQuestion/seeQuestion.resolvers.js
@@ -21,6 +21,24 @@ export default {
             ...(sort === "likes" && { likes: "desc" })
           }
         })
+      } else if (seeType === "tags") {
+        return client.question.findMany({
+          where: {
+            state: "public",
+            ...(search && { question: { contains: search } })
+          },
+          include: {
+            user: true,
+            tags: true
+          },
+          take: 10,
+          skip: page * 10 - 10,
+          orderBy: {
+            ...(sort === "recent" && { createdAt: "desc" }),
+            ...(sort === "hits" && { hits: "desc" }),
+            ...(sort === "likes" && { likes: "desc" })
+          }
+        })
       }
     }
   }
--- src/quiz/seeQuiz/seeQuiz.resolvers.js
+++ src/quiz/seeQuiz/seeQuiz.resolvers.js
@@ -2,12 +2,36 @@ import client from "../../client"
 
 export default {
   Query: {
-    seeQuiz: async (_, { seeType, page, search, sort }, { loggedInUser }) => {
+    seeQuiz: async (_, { seeType, page, search, sort, tags }, { loggedInUser }) => {
       if (seeType === "all") {
         return client.quiz.findMany({
           where: {
             state: "public",
             ...(search && { title: { contains: search } })
+          },
+          include: {
+            user: true,
+            tags: true
+          },
+          take: 10,
+          skip: page * 10 - 10,
+          orderBy: {
+            ...(sort === "recent" && { createdAt: "desc" }),
+            ...(sort === "hits" && { hits: "desc" }),
+            ...(sort === "likes" && { likes: "desc" })
+          }
+        })
+      } else if (seeType === "tags") {
+        const tagsArr = tags.split(",").map((item) => {
+          return {
+            tags: { some: { "name": item } }
+          }
+        })
+        return client.quiz.findMany({
+          where: {
+            state: "public",
+            ...(search && { title: { contains: search } }),
+            AND: tagsArr
           },
           include: {
             user: true,
