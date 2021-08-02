89707e77a964b2290eb0d548ec03f425ccc526b5
nlom0218 <nlom0218@naver.com>
2021-08-02 11:17:42 +0900

    detailQuiz

---
+++ src/quiz/detailQuiz/detailQuiz.resolvers.js
@@ -0,0 +1,12 @@
+import client from "../../client"
+
+export default {
+  Query: {
+    detailQuiz: async (_, { id }) => {
+      return await client.quiz.findUnique({
+        where: { id },
+        include: { user: true, tags: true, questions: true }
+      })
+    }
+  }
+}
