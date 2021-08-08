04e8b71f00ab4127270542f826de4ef7d99fc833
nlom0218 <nlom0218@naver.com>
2021-08-08 19:47:30 +0900

    deleteAccount

---
+++ src/user/deleteAccount/deleteAccount.resolvers.js
@@ -0,0 +1,54 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+import bcrypt from "bcrypt"
+import { deleteToS3 } from "../../shared/shared";
+
+export default {
+  Mutation: {
+    deleteAccount: protectedResolver(async (_, { username, password }, { loggedInUser }) => {
+      const user = await client.user.findUnique({ where: { username } })
+      if (user.id !== loggedInUser.id) {
+        return {
+          ok: false,
+          error: "탈퇴 권한이 없습니다."
+        }
+      }
+      const passwordOk = await bcrypt.compare(password, user.password)
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "비밀번호가 틀립니다."
+        }
+      }
+      if (user.avatarURL) {
+        await deleteToS3(user.avatarURL, "userProfile")
+      }
+      await client.quiz.deleteMany({
+        where: {
+          userId: user.id
+        }
+      })
+      await client.question.deleteMany({
+        where: {
+          userId: user.id
+        }
+      })
+      await client.quizLike.deleteMany({
+        where: {
+          userId: user.id
+        }
+      })
+      await client.questionLike.deleteMany({
+        where: {
+          userId: user.id
+        }
+      })
+      await client.user.delete({
+        where: { username }
+      })
+      return {
+        ok: true
+      }
+    })
+  }
+}
---
+++ src/user/deleteAccount/deleteAccount.typeDefs.js
@@ -0,0 +1,7 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type Mutation {
+    deleteAccount(username: String!, password: String!): mutationResult!
+  }
+`
