afe97963739f6e5ff2ae96e011785d9d1c2fd64d
nlom0218 <nlom0218@naver.com>
2021-08-21 18:51:22 +0900

    createStudentAccount user username

--- src/user/createStudentAccount/createStudentAccount.resolvers.js
+++ src/user/createStudentAccount/createStudentAccount.resolvers.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt"
 
 export default {
   Mutation: {
-    createStudentAccount: protectedResolver(async (_, { id, password, nickname }, { loggedInUser }) => {
+    createStudentAccount: protectedResolver(async (_, { id, password, nickname, username }, { loggedInUser }) => {
       const teacher = await client.user.findUnique({ where: { id } })
       if (!teacher) {
         return {
@@ -22,7 +22,10 @@ export default {
       for (let i = 0; i < nicknameArr.length; i++) {
         await client.user.create({
           data: {
-            username: `${teacher.username}_s${i + 1}`,
+            ...(username
+              ? { username: `${username}_s${i + 1}` }
+              : { username: `${teacher.username}_s${i + 1}` }
+            ),
             nickname: nicknameArr[i],
             type: "student",
             password: await bcrypt.hash(`${password}^^${i + 1}`, 10),
--- src/user/createStudentAccount/createStudentAccount.typeDefs.js
+++ src/user/createStudentAccount/createStudentAccount.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Mutation {
-    createStudentAccount(id: Int!, nickname: String!, password: String!): mutationResult!
+    createStudentAccount(id: Int!, nickname: String!, password: String!, username: String): mutationResult!
   }
 `
