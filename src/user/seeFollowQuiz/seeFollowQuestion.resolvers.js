213b06332f2eaa74bbfec93035a247442e9c9021
nlom0218 <nlom0218@naver.com>
2021-08-31 21:43:08 +0900

    seeFollowQuestion

---
+++ src/user/seeFollowQuiz/seeFollowQuestion.resolvers.js
@@ -0,0 +1,27 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Query: {
+    seeFollowQuestion: protectedResolver(async (_, { id, page }) => {
+      const quiz = await client.question.findMany({
+        where: {
+          followUser: { some: { id } }
+        },
+        take: 10,
+        skip: page * 10 - 10,
+        orderBy: { createdAt: "desc" },
+        include: { user: true, tags: true }
+      })
+      const totalNum = await client.question.count({
+        where: {
+          followUser: { some: { id } }
+        }
+      })
+      return {
+        question,
+        totalNum
+      }
+    })
+  }
+}
