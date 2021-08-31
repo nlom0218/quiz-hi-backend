927ffa6cfafaba8afac4be8aaaab26a66f9ff926
nlom0218 <nlom0218@naver.com>
2021-08-31 23:23:41 +0900

    unfollowQuiz

---
+++ src/quiz/unfollowQuiz/unfollowQuiz.resolvers.js
@@ -0,0 +1,21 @@
+import client from "../../client";
+import { protectedResolver } from "../../user/users.utils";
+
+export default {
+  Mutation: {
+    followQuiz: protectedResolver(async (_, { quizIds }, { loggedInUser }) => {
+      const quizArr = quizIds.split(",").map((item) => { return { id: parseInt(item) } })
+      await client.user.update({
+        where: { id: loggedInUser.id },
+        data: {
+          followQuiz: {
+            disconnect: quizArr
+          }
+        }
+      })
+      return {
+        ok: true
+      }
+    })
+  }
+}
