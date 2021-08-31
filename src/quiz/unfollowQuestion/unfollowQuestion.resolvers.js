396f28057ff7eb819e08e41880873004512fe626
nlom0218 <nlom0218@naver.com>
2021-08-31 23:24:57 +0900

    unfollowQuestion

--- src/quiz/unfollowQuestion/unfollowQuestion.resolvers.js
+++ src/quiz/unfollowQuestion/unfollowQuestion.resolvers.js
@@ -3,13 +3,13 @@ import { protectedResolver } from "../../user/users.utils";
 
 export default {
   Mutation: {
-    unfollowQuiz: protectedResolver(async (_, { quizIds }, { loggedInUser }) => {
-      const quizArr = quizIds.split(",").map((item) => { return { id: parseInt(item) } })
+    unfollowQuestion: protectedResolver(async (_, { questionIds }, { loggedInUser }) => {
+      const questionArr = questionIds.split(",").map((item) => { return { id: parseInt(item) } })
       await client.user.update({
         where: { id: loggedInUser.id },
         data: {
-          followQuiz: {
-            disconnect: quizArr
+          followQuestion: {
+            disconnect: questionArr
           }
         }
       })
