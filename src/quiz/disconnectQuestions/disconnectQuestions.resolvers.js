f815dfe208ddcaf6b5c83cf968586d80b549a871
nlom0218 <nlom0218@naver.com>
2021-09-04 17:41:30 +0900

    disconnectQuestions quiz order

--- src/quiz/disconnectQuestions/disconnectQuestions.resolvers.js
+++ src/quiz/disconnectQuestions/disconnectQuestions.resolvers.js
@@ -5,6 +5,7 @@ export default {
   Mutation: {
     disconnectQuestions: protectedResolver((async (_, { quizId, questionsId }, { loggedInUser }) => {
       const quiz = await client.quiz.findUnique({ where: { id: quizId } })
+      const quizOrder = JSON.parse(quiz.order)
       if (!quiz) {
         return {
           ok: false,
@@ -21,12 +22,25 @@ export default {
       const disconnectArr = questionsIdArr.map((item) => {
         return { id: item }
       })
+
+      let order = null
+      if (quiz.order) {
+        order = JSON.stringify(quizOrder.map((item) => {
+          if (questionsIdArr.includes(item)) {
+            return undefined
+          } else {
+            return item
+          }
+        }).filter((item) => item !== undefined))
+      }
+
       await client.quiz.update({
         where: { id: quizId },
         data: {
           questions: {
             disconnect: disconnectArr
-          }
+          },
+          order
         }
       })
       return {
