6d1d5ff4cd3645c20b10692bf145ffb313a7ea7d
nlom0218 <nlom0218@naver.com>
2021-09-06 19:13:46 +0900

    finishHomework update student quizSocre

--- src/homework/finishHomework/finishHomework.resolvers.js
+++ src/homework/finishHomework/finishHomework.resolvers.js
@@ -1,17 +1,41 @@
 import client from "../../client";
+import { compare } from "../../shared/shared";
 import { protectedResolver } from "../../user/users.utils";
 
 export default {
   Mutation: {
-    finishHomework: protectedResolver(async (_, { homeworkId }, { loggedInUser }) => {
+    finishHomework: protectedResolver(async (_, { homeworkId, totalScore }, { loggedInUser }) => {
       const teacher = await client.user.findUnique({ where: { id: loggedInUser.id } })
-      const homework = await client.homework.findUnique({ where: { id: homeworkId } })
+      const homework = await client.homework.findUnique({
+        where: { id: homeworkId },
+        include: { user: true }
+      })
       if (teacher.id !== homework.teacherId) {
         return {
           ok: false,
           error: "숙제 삭제 권한이 없습니다."
         }
       }
+
+      if (homework.mode === "cooperation") {
+        if (totalScore >= homework.targetScore) {
+          const studentArr = homework.user
+          for (let i = 0; i < studentArr.length; i++) {
+            const quizScoreArr = JSON.parse(studentArr[i].quizScore)
+            const homeworkScore = quizScoreArr.filter((item) => item.order === homework.order)[0]
+            const existArr = quizScoreArr.filter((item) => item.order !== homework.order)
+            const newHomeworkScore = { ...homeworkScore, ...{ score: homeworkScore.score + teacher.cooperationScore } }
+            const newQuizScore = JSON.stringify([...existArr, newHomeworkScore].sort(compare("order")))
+            await client.user.update({
+              where: { id: studentArr[i].id },
+              data: {
+                quizScore: newQuizScore
+              }
+            })
+          }
+        }
+      }
+
       await client.homework.update({
         where: { id: homeworkId },
         data: {
--- src/homework/finishHomework/finishHomework.typeDefs.js
+++ src/homework/finishHomework/finishHomework.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core"
 
 export default gql`
   type Mutation {
-    finishHomework(homeworkId: Int!): mutationResult
+    finishHomework(homeworkId: Int!, totalScore: Int): mutationResult
   }
 `
