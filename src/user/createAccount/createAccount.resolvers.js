adea5e6789ed5c45575b7cd3fdf905fc8797c339
nlom0218 <nlom0218@naver.com>
2021-09-09 21:51:09 +0900

    createAccount existingUser error

--- src/user/createAccount/createAccount.resolvers.js
+++ src/user/createAccount/createAccount.resolvers.js
@@ -8,13 +8,10 @@ export default {
         const existingUser = await client.user.findUnique({
           where: { username }
         })
-        const existingUser2 = await client.user.findUnique({
-          where: { email }
-        })
-        if (existingUser || existingUser2) {
+        if (existingUser) {
           return {
             ok: false,
-            error: "아이디 또는 이메일이 이미 존재합니다."
+            error: "아이디가 이미 존재합니다."
           }
         }
 
