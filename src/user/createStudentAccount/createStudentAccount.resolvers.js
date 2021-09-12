333658dae8f0f6fa9122c2069b84a4e5774a6bcc
nlom0218 <nlom0218@naver.com>
2021-09-12 10:51:06 +0900

    createStudentAccount studentsOfTeacher

--- src/user/createStudentAccount/createStudentAccount.resolvers.js
+++ src/user/createStudentAccount/createStudentAccount.resolvers.js
@@ -5,7 +5,8 @@ import bcrypt from "bcrypt"
 export default {
   Mutation: {
     createStudentAccount: protectedResolver(async (_, { id, password, nickname, username }, { loggedInUser }) => {
-      const teacher = await client.user.findUnique({ where: { id } })
+      const teacher = await client.user.findUnique({ where: { id }, include: { students: true } })
+      const studentsOfTeacehr = teacher.students.filter((item) => item.username.split("_")[0] === teacher.username)
       if (!teacher) {
         return {
           ok: false,
@@ -19,38 +20,14 @@ export default {
         }
       }
 
-      if (username) {
-        const existTeacherUsername = await client.user.findUnique({
-          where: { username }
-        })
-        if (existTeacherUsername) {
-          return {
-            ok: false,
-            error: `사용할 수 없는 아이디입니다.`
-          }
-        }
-        const existUsername = await client.user.findUnique({
-          where: { username: `${username}_s1` }
-        })
-        if (existUsername) {
-          return {
-            ok: false,
-            error: `${username}_s(학생번호)의 아이디가 존재합니다.`
-          }
-        }
-      }
-
       const nicknameArr = nickname.split(",")
       for (let i = 0; i < nicknameArr.length; i++) {
         await client.user.create({
           data: {
-            ...(username
-              ? { username: `${username}_s${i + 1}` }
-              : { username: `${teacher.username}_s${i + 1}` }
-            ),
+            username: `${teacher.username}_s${i + studentsOfTeacehr.length + 1}`,
             nickname: nicknameArr[i],
             type: "student",
-            password: await bcrypt.hash(`${password}^^${i + 1}`, 10),
+            password: await bcrypt.hash(`${password}^^${i + studentsOfTeacehr.length + 1}`, 10),
             teacher: {
               connect: { id: teacher.id }
             }
