82a52cb1ec82f01b8bffdcdc949a533c6e6f450b
nlom0218 <nlom0218@naver.com>
2021-09-17 23:24:56 +0900

    deleteAllHomework deleteMay homeworkResult

--- src/homework/deleteAllHomework/deleteAllHomework.resolvers.js
+++ src/homework/deleteAllHomework/deleteAllHomework.resolvers.js
@@ -4,6 +4,18 @@ import { protectedResolver } from "../../user/users.utils";
 export default {
   Mutation: {
     deleteAllHomework: protectedResolver(async (_, { teacherId }) => {
+      const homework = await client.homework.findMany({
+        where: {
+          teacherId
+        }
+      })
+      for (let i = 0; i < homework.length; i++) {
+        await client.homeworkResult.deleteMany({
+          where: {
+            homeworkId: homework[i].id
+          }
+        })
+      }
       await client.homework.deleteMany({
         where: {
           teacherId
