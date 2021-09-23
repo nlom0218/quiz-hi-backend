2749519f970cc6a5dcb041fefc1df48067c8ea40
nlom0218 <nlom0218@naver.com>
2021-09-23 21:11:09 +0900

    seeFollowing

---
+++ src/user/seeFollowing/seeFollowing.resolvers.js
@@ -0,0 +1,20 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Query: {
+    seeFollowing: protectedResolver(async (_, { userId, page }) => {
+      return await client.user.findMany({
+        where: {
+          followers: {
+            some: {
+              id: userId,
+            }
+          }
+        },
+        take: 10,
+        skip: page * 10 - 10,
+      })
+    })
+  }
+}
