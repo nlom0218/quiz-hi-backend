8962ccbd4086589bb6e85cec1518f02a11e4e3ab
nlom0218 <nlom0218@naver.com>
2021-09-23 19:45:08 +0900

    seeFollower

---
+++ src/user/seeFollower/seeFollower.resolvers.js
@@ -0,0 +1,20 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Query: {
+    seeFollower: protectedResolver(async (_, { userId, page }) => {
+      return await client.user.findMany({
+        where: {
+          following: {
+            some: {
+              id: userId,
+            }
+          }
+        },
+        take: 10,
+        skip: page * 10 - 10,
+      })
+    })
+  }
+}
