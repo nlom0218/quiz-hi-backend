13fef27543cbbc9f24342809bb8b597c2a265e4c
nlom0218 <nlom0218@naver.com>
2021-10-03 17:11:47 +0900

    seeFollowQuiz nomal user

--- src/user/seeFollowQuiz/seeFollowQuiz.resolvers.js
+++ src/user/seeFollowQuiz/seeFollowQuiz.resolvers.js
@@ -4,23 +4,38 @@ import { protectedResolver } from "../users.utils";
 export default {
   Query: {
     seeFollowQuiz: protectedResolver(async (_, { id, page }) => {
-      const quiz = await client.quiz.findMany({
-        where: {
-          followUser: { some: { id } }
-        },
-        take: 10,
-        skip: page * 10 - 10,
-        // orderBy: { createdAt: "desc" },
-        include: { user: true, tags: true }
-      })
-      const totalNum = await client.quiz.count({
-        where: {
-          followUser: { some: { id } }
+      const user = await client.user.findUnique({ where: { id } })
+      if (user.type === "nomal") {
+        const quiz = await client.quiz.findMany({
+          where: { user: { id } },
+          orderBy: { createdAt: "desc" },
+          take: 10,
+          skip: page * 10 - 10,
+        })
+        const totalNum = await client.quiz.count({ where: { user: { id } } })
+        return {
+          quiz,
+          totalNum
         }
-      })
-      return {
-        quiz,
-        totalNum
+      } else {
+        const quiz = await client.quiz.findMany({
+          where: {
+            followUser: { some: { id } }
+          },
+          take: 10,
+          skip: page * 10 - 10,
+          // orderBy: { createdAt: "desc" },
+          include: { user: true, tags: true }
+        })
+        const totalNum = await client.quiz.count({
+          where: {
+            followUser: { some: { id } }
+          }
+        })
+        return {
+          quiz,
+          totalNum
+        }
       }
     })
   }
