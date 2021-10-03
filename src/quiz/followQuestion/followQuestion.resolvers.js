94e28fa0566320c61e7bb9bb0ba4d479383d139a
nlom0218 <nlom0218@naver.com>
2021-10-03 16:58:12 +0900

    followQuiz, followQuestion user type

--- src/quiz/followQuestion/followQuestion.resolvers.js
+++ src/quiz/followQuestion/followQuestion.resolvers.js
@@ -4,6 +4,12 @@ import { protectedResolver } from "../../user/users.utils";
 export default {
   Mutation: {
     followQuestion: protectedResolver(async (_, { questionIds }, { loggedInUser }) => {
+      if (loggedInUser.type !== "teacher") {
+        return {
+          ok: false,
+          error: "장바구니 기능은 선생님 계정에서만 이용가능합니다."
+        }
+      }
       const questionArr = questionIds.split(",").map((item) => { return { id: parseInt(item) } })
       await client.user.update({
         where: { id: loggedInUser.id },
--- src/quiz/followQuiz/followQuiz.resolvers.js
+++ src/quiz/followQuiz/followQuiz.resolvers.js
@@ -4,6 +4,12 @@ import { protectedResolver } from "../../user/users.utils";
 export default {
   Mutation: {
     followQuiz: protectedResolver(async (_, { quizIds }, { loggedInUser }) => {
+      if (loggedInUser.type !== "teacher") {
+        return {
+          ok: false,
+          error: "장바구니 기능은 선생님 계정에서만 이용가능합니다."
+        }
+      }
       const quizArr = quizIds.split(",").map((item) => { return { id: parseInt(item) } })
       await client.user.update({
         where: { id: loggedInUser.id },
