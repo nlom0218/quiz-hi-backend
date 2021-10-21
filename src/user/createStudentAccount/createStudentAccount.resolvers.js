26b7eac158e2e063f57641f15822a859b7c1af33
nlom0218 <nlom0218@naver.com>
2021-10-21 22:39:46 +0900

    createStudnetAccount lastStudnet num

--- src/user/createStudentAccount/createStudentAccount.resolvers.js
+++ src/user/createStudentAccount/createStudentAccount.resolvers.js
@@ -6,7 +6,15 @@ export default {
   Mutation: {
     createStudentAccount: protectedResolver(async (_, { id, password, nickname, username }, { loggedInUser }) => {
       const teacher = await client.user.findUnique({ where: { id }, include: { students: true } })
+
+      // 학생 마지막 번호
       const studentsOfTeacher = teacher.students.filter((item) => item.username.split("_")[0] === teacher.username)
+      const studentsNum = studentsOfTeacher.map((item) => {
+        return parseInt(item.username.split("_s")[1])
+      })
+        .sort((a, b) => b - a);
+      const lastStudentNum = studentsNum[0]
+
       if (!teacher) {
         return {
           ok: false,
@@ -24,10 +32,10 @@ export default {
       for (let i = 0; i < nicknameArr.length; i++) {
         await client.user.create({
           data: {
-            username: `${teacher.username}_s${i + studentsOfTeacher.length + 1}`,
+            username: `${teacher.username}_s${i + lastStudentNum + 1}`,
             nickname: nicknameArr[i],
             type: "student",
-            password: await bcrypt.hash(`${password}^^${i + studentsOfTeacher.length + 1}`, 10),
+            password: await bcrypt.hash(`${password}^^${i + lastStudentNum + 1}`, 10),
             teacher: {
               connect: { id: teacher.id }
             }
